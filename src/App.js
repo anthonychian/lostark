8adeda0f4b19595db86ac7ffb0c5e206dd59fa4f
Anthony C <achian@ucsc.edu>
2022-03-12 10:19:33 -0800

    add font

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Banner from "./components/Banner"
 export default function App() {
   return (
     <>
-      {/* <Banner /> */}
+      <Banner />
       <Tooltip />
       <SkillList />
     </>
