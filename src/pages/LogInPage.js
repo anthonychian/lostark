7fa1a61a40d185dd13d4a689b0a9714c84446267
Anthony C <anthonychian@gmail.com>
2025-08-12 23:14:37 -0700

    node22

--- src/pages/LogInPage.js
+++ src/pages/LogInPage.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Banner from "../components/Banner"
-import Login from '../components/Login'
+import React from "react";
+import Banner from "../components/Banner";
+import Login from "../components/Login";
 
 export default function LogInPage() {
   return (
     <>
-        <Banner />
-        <Login />
+      <Banner />
+      <Login />
     </>
-  )
+  );
 }
